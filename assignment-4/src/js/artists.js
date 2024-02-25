88275176ddd42ae471d6eb1330e3b189f21fbaa0
Joon <thdong1@myseneca.ca>
2024-02-24 21:36:34 -0500

    Adding songs

--- assignment-4/src/js/artists.js
+++ assignment-4/src/js/artists.js
@@ -10,7 +10,7 @@ window.artists = [
     artistID: "AID-1",
     name: "Oneheart",
     url: [
-      { url: "https://www.instagram.com/oneheartisok/?hl=en", name: "Instagram" },
+      { url: "https://www.instagram.com/oneheartisok/", name: "Instagram" },
       { url: "https://www.youtube.com/@oneheartt", name: "Youtube" }
     ]
   }
@@ -18,16 +18,16 @@ window.artists = [
     artistID: "AID-2",
     name: "Antent",
     url: [
-      { url: "https://www.instagram.com/oneheartisok/?hl=en", name: "Instagram" },
-      { url: "https://www.youtube.com/@oneheartt", name: "Youtube" }
+      { url: "https://www.instagram.com/antentofficial/", name: "Instagram" },
+      { url: "https://www.youtube.com/@Antent", name: "Youtube" }
     ]
   }
   {
     artistID: "AID-3",
     name: "lmnl",
     url: [
-      { url: "https://www.instagram.com/oneheartisok/?hl=en", name: "Instagram" },
-      { url: "https://www.youtube.com/@oneheartt", name: "Youtube" }
+      { url: "https://www.instagram.com/yourpastreality/", name: "Instagram" },
+      { url: "https://www.youtube.com/channel/UCeQwynIb6moziR0I2xnKwYwt", name: "Youtube" }
     ]
   }
 ];
--- assignment-4/src/js/songs.js
+++ assignment-4/src/js/songs.js
@@ -6,4 +6,121 @@
 
 window.songs = [
   /* TODO */
+  {
+    songId: "SID-1",
+    artistId: "AID-1",
+    title: "snowfall",
+    year: "2022",
+    duration: 124,
+    url: "https://www.youtube.com/watch?v=LlN8MPS7KQs",
+    explicit: false
+  }
+  {
+    songId: "SID-2",
+    artistId: "AID-1",
+    title: "apathy",
+    year: "2022",
+    duration: 118,
+    url: "https://www.youtube.com/watch?v=FgjA9EuxIlY",
+    explicit: false
+  }
+  {
+    songId: "SID-3",
+    artistId: "AID-1",
+    title: "this feeling",
+    year: "2022",
+    duration: 94,
+    url: "https://www.youtube.com/watch?v=J4RxO3lRsi0",
+    explicit: false
+  }
+  {
+    songId: "SID-4",
+    artistId: "AID-1",
+    title: "watching the stars",
+    year: "2022",
+    duration: 99,
+    url: "https://www.youtube.com/watch?v=C42iSY6Y9g0",
+    explicit: false
+  }
+  {
+    songId: "SID-5",
+    artistId: "AID-1",
+    title: "distorted memories",
+    year: "2022",
+    duration: 110,
+    url: "https://www.youtube.com/watch?v=gSwi1Tk1gV8",
+    explicit: false
+  }
+  {
+    songId: "SID-6",
+    artistId: "AID-1",
+    title: "touch",
+    year: "2023",
+    duration: 132,
+    url: "https://www.youtube.com/watch?v=mmawPTFvvd8",
+    explicit: true
+  }
+  {
+    songId: "SID-7",
+    artistId: "AID-1",
+    title: "next to you",
+    year: "2023",
+    duration: 123,
+    url: "https://www.youtube.com/watch?v=dJL229efkbk",
+    explicit: true
+  }
+  {
+    songId: "SID-8",
+    artistId: "AID-2",
+    title: "i'll be your reason",
+    year: "2023",
+    duration: 138,
+    url: "https://www.youtube.com/watch?v=_EJL1XIh0d4",
+    explicit: false
+  }
+  {
+    songId: "SID-9",
+    artistId: "AID-2",
+    title: "where are you?",
+    year: "2022",
+    duration: 137,
+    url: "https://www.youtube.com/watch?v=hHTmlTiagLI",
+    explicit: false
+  }
+  {
+    songId: "SID-10",
+    artistId: "AID-2",
+    title: "hope to see you again",
+    year: "2023",
+    duration: 147,
+    url: "https://www.youtube.com/watch?v=r9k74AGYZoU",
+    explicit: false
+  }
+  {
+    songId: "SID-11",
+    artistId: "AID-2",
+    title: "snowflakes",
+    year: "2023",
+    duration: 124,
+    url: "https://www.youtube.com/watch?v=mrdmGAGIbXA",
+    explicit: false
+  }
+  {
+    songId: "SID-12",
+    artistId: "AID-2",
+    title: "weariness",
+    year: "2023",
+    duration: 129,
+    url: "https://www.youtube.com/watch?v=bWq2B5SRHR0",
+    explicit: false
+  }
+  {
+    songId: "SID-13",
+    artistId: "AID-2",
+    title: "Missing Hope",
+    year: "2023",
+    duration: 201,
+    url: "https://www.youtube.com/watch?v=4hqqk0zlz_M",
+    explicit: true
+  }
 ];
