2765229b615f96b2cfb39031d036ea987ca06dd9
Joon <thdong1@myseneca.ca>
2024-02-26 00:36:40 -0500

    Adding dynamic song data

--- assignment-4/src/js/app.js
+++ assignment-4/src/js/app.js
@@ -20,37 +20,69 @@ const { artists, songs } = window;
 // For debugging, display all of our data in the console. You can remove this later.
 console.log({ artists, songs }, "App Data");
 
+function createArtistButton(artist) {
+  let artistButton = document.createElement("button");
+  artistButton.innerText = artist.name;
+  return artistButton;
+}
+
+function appendArtistButtonToNav(artistButton) {
+  let nav = document.querySelector("#menu");
+  nav.appendChild(artistButton);
+}
+
+function displayArtistInfo(artistButton, artist) {
+  artistButton.addEventListener("click", function () {
+    let selectedArtist = document.querySelector("#selected-artist");
+    selectedArtist.innerText = artist.name + " (";
+    createSocialLinks(artist, selectedArtist);
+  });
+}
+
+function createSocialLinks(artist, selectedArtist) {
+  artist.url.forEach(function (link, index) {
+    let socials = document.createElement("a");
+    socials.href = link.url;
+    socials.target = "_blank";
+
+    if (index < artist.url.length - 1) {
+      socials.textContent = link.name + ",";
+    } else {
+      socials.textContent = link.name;
+    }
+
+    selectedArtist.appendChild(socials);
+
+    if (index < artist.url.length - 1) {
+      selectedArtist.appendChild(document.createTextNode(" "));
+    }
+  });
+}
+
+function displaySongs() {
+  window.songs.forEach(function (songs) {});
+}
+
+function makeButtons() {
+  window.artists.forEach(function (artist) {
+    let artistButton = createArtistButton(artist);
+    appendArtistButtonToNav(artistButton);
+    displayArtistInfo(artistButton, artist);
+  });
+}
+
+/*
+window.songs.forEach(function (song) {
+  if (song.artistId === 'AID-1') {
+  let table = document.querySelector('#songs');
+  let row = document.createElement("tr");
+  row.innerText = song.title;
+  table.appendChild(row);
+}
+*/
+
 //Ensure everything happens on DOM load up
 window.onload = function () {
   //For each artist object in the artist array
-  window.artists.forEach(function (artist) {
-    //Make a button for the artist and dynamically add it to nav
-    let artistButton = document.createElement("button");
-    let nav = document.querySelector("#menu");
-    artistButton.innerText = artist.name;
-    nav.appendChild(artistButton);
-
-    //Each button has an onclick function
-    artistButton.addEventListener("click", function () {
-      let selectedArtist = document.querySelector("#selected-artist");
-      //Display the artist name and socials at the selected artist id
-      selectedArtist.innerText = artist.name + " (";
-      artist.url.forEach(function (link) {
-        let socials = document.createElement("a");
-        socials.href = link.url;
-        //Formatting
-        //If the index of the current url information is not the last
-        if (artist.url.indexOf(link) !== artist.url.length - 1) {
-          socials.textContent = link.name + ",";
-          selectedArtist.appendChild(socials);
-          selectedArtist.appendChild(document.createTextNode(" "));
-          //Else if it is the last index
-        } else {
-          socials.textContent = link.name;
-          selectedArtist.appendChild(socials);
-          selectedArtist.appendChild(document.createTextNode(")"));
-        }
-      });
-    });
-  });
+  makeButtons();
 };
